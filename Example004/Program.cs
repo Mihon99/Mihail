2c9f04651adfb29321cb57320a3e17bf928bdd07
reaper <reaper>
2022-10-02 09:58:46 +0300

    Example 00402

--- Example004/Program.cs
+++ Example004/Program.cs
@@ -4,3 +4,8 @@ Console.WriteLine(numberA / numberB);
 double numberC = 12;
 double numberF = 5;
 Console.WriteLine(numberC / numberF);
+int numberD = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberD);
+int numberV = new Random().Next(1, 10);
+Console.WriteLine(numberV);
+Console.WriteLine(numberD + numberV);
